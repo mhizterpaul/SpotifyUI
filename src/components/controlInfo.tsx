81110eed650f72579a016f15ec4faf37f9ae9bf2
mhizterpaul <chinonsopaul@gmail.com>
2023-07-30 21:12:14 +0100

    update AsyncHelper

--- src/components/controlInfo.tsx
+++ src/components/controlInfo.tsx
@@ -1,7 +1,7 @@
 const ControlInfo = () => {
     return (
         <footer className='bg-[#1ed760]'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="1.5"><path d="M1 13.857v-3.714a2 2 0 0 1 2-2h2.9a1 1 0 0 0 .55-.165l6-3.956a1 1 0 0 1 1.55.835v14.286a1 1 0 0 1-1.55.835l-6-3.956a1 1 0 0 0-.55-.165H3a2 2 0 0 1-2-2Z" /><path strokeLinecap="round" strokeLinejoin="round" d="M17.5 7.5S19 9 19 11.5s-1.5 4-1.5 4m3-11S23 7 23 11.5s-2.5 7-2.5 7" /></g></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" strokeWidth="1.5"><path d="M1 13.857v-3.714a2 2 0 0 1 2-2h2.9a1 1 0 0 0 .55-.165l6-3.956a1 1 0 0 1 1.55.835v14.286a1 1 0 0 1-1.55.835l-6-3.956a1 1 0 0 0-.55-.165H3a2 2 0 0 1-2-2Z" /><path strokeLinecap="round" strokeLinejoin="round" d="M17.5 7.5S19 9 19 11.5s-1.5 4-1.5 4m3-11S23 7 23 11.5s-2.5 7-2.5 7" /></g></svg>
 
         </footer>
     )
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,4 +1,6 @@
 import React, { ReactNode } from 'react'
+import axios from 'axios'
+import credentials from '../utilities/apiKeys'
 
 export default class Sidebar extends React.Component<{ open?: boolean }> {
     navRef: React.RefObject<HTMLElement>;
@@ -38,15 +40,15 @@ export default class Sidebar extends React.Component<{ open?: boolean }> {
                         </div>
                         your episodes</li>
                 </ul>
-                <ul>
-                    {/*fetch playlist using api */}
+                <FeaturedPlaylist/>
+                {/*<ul>
                     <li>fav</li>
                     <li>daily mix 1</li>
                     <li>discover weekly</li>
                     <li>Malayalam</li>
                     <li>dance/electronic mix</li>
                     <li>edm/popular</li>
-                </ul>
+                </ul>*/}
                 <button className='capitalize'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m11 12.2l-.9-.9q-.275-.275-.7-.275t-.7.275q-.275.275-.275.7t.275.7l2.6 2.6q.3.3.7.3t.7-.3l2.6-2.6q.275-.275.275-.7t-.275-.7q-.275-.275-.7-.275t-.7.275l-.9.9V9q0-.425-.288-.713T12 8q-.425 0-.713.288T11 9v3.2Zm1 9.8q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Zm0-2q3.35 0 5.675-2.325T20 12q0-3.35-2.325-5.675T12 4Q8.65 4 6.325 6.325T4 12q0 3.35 2.325 5.675T12 20Zm0-8Z" /></svg>
                     install app
@@ -55,3 +57,28 @@ export default class Sidebar extends React.Component<{ open?: boolean }> {
         </section>)
     }
 }
+
+const FeaturedPlaylist = () => {
+   const { clientID, clientSecret } = JSON.parse(credentials),
+   client = async () => await Client.create({ token: { clientID, clientSecret } });
+   
+   console.log(client());
+   return null
+}
+/*const 
+    const [playlists, setPlaylists] = React.useState(null),
+        baseURL = 'https://api.spotify.com/v1/browse/featured-playlists';
+    React.useEffect(() => {
+        axios.get(baseURL).then(res => {
+            setPlaylists(res.data);
+        })
+    }, [])
+
+    if (!playlists) return null;
+
+    return (
+        <div>
+            <h1>{playlists.title}</h1>
+            <p>{playlists.body}</p>
+        </div>
+    ); */
