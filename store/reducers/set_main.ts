cf2ac84543d0f1a171664e7e7477ac8bc21df573
mhizterpaul <chinonsopaul@gmail.com>
2023-08-26 00:13:12 +0100

    typo

--- store/reducers/set_main.ts
+++ store/reducers/set_main.ts
@@ -3,7 +3,7 @@
 export default function SET_MAIN(state = {href: '/', history: []}, action: {type: string}) {
     if(action.type) return {
         href: action.type,
-        history: [...state.history, action.href]
+        history: [...state.history, action.type]
     }
     return state;
 
