1f966c1788cd467ac01afacd3955c83c0e90e164
mhizterpaul <chinonsopaul@gmail.com>
2023-08-28 10:11:00 +0100

    ui code

--- components/network_request.tsx
+++ components/network_request.tsx
@@ -4,9 +4,8 @@ import { ApiStatus } from '@/store/reducers/access_token_slice'
 
 function Loader({ status, meta }: { status: ApiStatus, meta: string }) {
     return (
-        <div className='spinner'>
-            {meta + " " + status}
-        </div>
+        status === 'PENDING' ? <div className='spinner'></div>
+            : <div> {status + " " + meta}</div>
     )
 }
 
