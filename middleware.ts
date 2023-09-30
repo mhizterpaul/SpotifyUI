20a39c9c3fd5c2ce2e0c439b526c6a7dd17d6b28
mhizterpaul <chinonsopaul@gmail.com>
2023-09-30 08:54:56 +0100

    fix api

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { store } from '@/store'
-import { pushRef } from './store/reducers/main_slice';
+
  
 export function middleware({nextUrl}: NextRequest) {
     if(!nextUrl.pathname.startsWith('/_next') && nextUrl.pathname !== '/' && !nextUrl.pathname.startsWith('/?') && !nextUrl.pathname.startsWith('/api')){
